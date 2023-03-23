f30bb22395074e1af5cc579418822a9da1adb286
Qhubaib <19301054qhubaib@viva-technology.org>
2023-03-23 20:29:34 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,29 @@
+<?php
+
+// Set the recipient email address(es)
+$to = 'qhubaibshaikh56@gmail.com';
+
+// Set the email subject
+$subject = 'New Contact Form Submission';
+
+// Get the form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['form-message'];
+
+// Construct the email body
+$body = "Name: $name\nEmail: $email\nMessage: $message";
+
+// Set the email headers
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+
+// Send the email
+mail($to, $subject, $body, $headers);
+
+// Redirect back to the contact form page
+header('Location: index.html');
+
+?>
